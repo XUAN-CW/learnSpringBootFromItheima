8d77170ae9a66046d63253ef19cfc27496c76fc7
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-10-26 18:21:16 +0800

    java配置应用

---
+++ heima-springboot/src/main/java/com/itheima/config/JdbcConfig.java
@@ -0,0 +1,38 @@
+package com.itheima.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author XUAN
+ * @date 2020/10/26 - 17:45
+ * @references
+ * @purpose
+ * @errors
+ */
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class JdbcConfig {
+    @Value("${jdbc.url}")
+    String url;
+    @Value("${jdbc.driverClassName}")
+    String driverClassName;
+    @Value("${jdbc.username}")
+    String username;
+    @Value("${jdbc.password}")
+    String password;
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
--- heima-springboot/src/main/java/com/itheima/controller/HelloController.java
+++ heima-springboot/src/main/java/com/itheima/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.itheima.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,16 @@ import javax.sql.DataSource;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    // http://localhost:8080/helloConfig
+    @GetMapping("helloConfig")
+    public String helloConfig() {
+        System.out.println("dataSource = " + dataSource);
+        return "Hello, Spring Boot!";
+    }
 
     // http://localhost:8080/hello
     @GetMapping("hello")
