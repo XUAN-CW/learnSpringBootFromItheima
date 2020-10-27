0568c4297e141e6b3dababc33315b4cc0d47325f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-10-27 23:55:16 +0800

    多个Yaml配置文件

--- heima-springboot/src/main/java/com/itheima/controller/HelloController.java
+++ heima-springboot/src/main/java/com/itheima/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.itheima.controller;
 
 import com.itheima.config.JdbcProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,19 @@ public class HelloController {
 
     @Autowired
     private JdbcProperties prop;
+
+    @Value("${itcast.url}")
+    private String itcastUrl;
+
+    @Value("${itheima.url}")
+    private String itheimaUrl;
+
+    // http://localhost:8080/helloMultiYml
+    @GetMapping("helloMultiYml")
+    public String helloMultiYml() {
+        return " itcastUrl = " + itcastUrl + "<br>" +" itheimaUrl = " + itheimaUrl;
+    }
+
 
     // http://localhost:8080/helloYml
     @GetMapping("helloYml")
